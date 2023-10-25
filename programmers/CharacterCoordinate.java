c310f1dbbab0d6d5d86b5010902dc14767e212e3
GyeongHwa <gyeonghwaa_@naver.com>
2023-10-25 17:02:30 +0900

    캐릭터 좌표 /모든경우의 수 생각하기

---
+++ programmers/CharacterCoordinate.java
@@ -0,0 +1,40 @@
+package programmers;
+
+public class CharacterCoordinate {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = new int[board.length];
+        int u = 0;
+        int r = 0;
+        for (String word : keyinput) {
+            if (word.equals("up")) {
+                if (u >= (board[1] / 2)) {
+                    continue;
+                }
+                u++;
+            }
+            if (word.equals("down")) {
+                if (u < -(board[1] / 2)) {
+                    continue;
+                }
+                u--;
+            }
+            if (word.equals("left")) {
+                if (r < -(board[0] / 2)) {
+                    continue;
+                }
+                r--;
+            }
+            if (word.equals("right")) {
+                if (r >= (board[0] / 2)) {
+                    continue;
+                }
+                r++;
+            }
+        }
+        answer[0] = r;
+        answer[1] = u;
+
+        return answer;
+    }
+}
+
