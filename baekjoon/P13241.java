a3ceeb85f022df795a7b4be13cb416b85746cdcd
GyeongHwa <gyeonghwaa_@naver.com>
2023-11-10 13:09:21 +0900

    유클리드 호제법

---
+++ baekjoon/P13241.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class P13241
+{
+    public static long getGCD(long num1,long num2){
+        if(num1%num2==0){
+            return num2;
+        }
+        return getGCD(num2,num1%num2);
+    }
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        long num1= sc.nextLong();
+        long num2= sc.nextLong();
+
+
+        System.out.println((num1*num2)/getGCD(num1,num2));
+    }
+}
---
+++ baekjoon/P1934.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class P1934
+{//유클리드 호제법 A*B/최대공약수 =최소 공배수
+    public static int getGCD(int num1,int num2){
+        if(num1%num2==0){
+            return num2;
+        }
+        return getGCD(num2,num1%num2);
+    }
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+
+        for(int i=0;i<t;i++){
+            int num1=sc.nextInt();
+            int num2= sc.nextInt();
+            System.out.println((num1*num2)/getGCD(num1,num2));
+        }
+    }
+}
