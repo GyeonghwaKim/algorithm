584e33ebea4ab10c6c9fb7e70f2076150b46a8f4
GyeongHwa <gyeonghwaa_@naver.com>
2023-11-10 14:20:40 +0900

    규칙 찾기가 참 어렵

---
+++ baekjoon/P13909.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class P13909
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        long n=sc.nextLong();
+
+        System.out.println((int)Math.sqrt(n));
+
+    }
+}
