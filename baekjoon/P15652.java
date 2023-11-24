d792a2cd7f93d7f1e709c3c68f2ee0e2810b094f
GyeongHwa <gyeonghwaa_@naver.com>
2023-11-24 14:28:41 +0900

    백준 백트래킹 중 dfs

---
+++ baekjoon/P15652.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P15652 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        arr=new int[M];
+        dfs(0,1);
+        System.out.println(sb);
+    }
+
+    static int N;
+    static int M;
+    static int[] arr;
+
+    static StringBuilder sb=new StringBuilder();
+
+    static void dfs(int depth,int num){
+        if(depth==M){
+            for(int val:arr) sb.append(val).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=num;i<=N;i++){
+            arr[depth]=i;
+            dfs(depth+1,i);
+        }
+    }
+
+
+}
